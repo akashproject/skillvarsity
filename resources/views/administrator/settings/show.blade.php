adb06f6e1af492f06d4cf1ca992e84eb0f4c3e88
akashproject <akashdutta.scriptcrown@gmail.com>
2025-01-13 13:53:44 +0530

    update 2024

--- resources/views/administrator/settings/show.blade.php
+++ resources/views/administrator/settings/show.blade.php
@@ -50,96 +50,11 @@
 							class="nav-link"
 							role="tab"
 							data-bs-toggle="tab"
-							data-bs-target="#navs-pills-justified-messages"
-							aria-controls="navs-pills-justified-messages"
+							data-bs-target="#navs-pills-justified-utm"
+							aria-controls="navs-pills-justified-utm"
 							aria-selected="false"
 						>
-							<i class="tf-icons bx bx-message-square"></i> Social Settings
-						</button>
-					</li>
-				</ul>
-				<div class="tab-content">
-					<div class="tab-pane fade show active" id="navs-pills-justified-home" role="tabpanel">
-						<h5 class="card-header">General</h5>
-						<!-- Account -->
-						<div class="card-body">
-							<div class="d-flex align-items-start align-items-sm-center gap-4">
-								<img
-									src="../assets/img/avatars/1.png"
-									alt="user-avatar"
-									class="d-block rounded"
-									height="100"
-									width="100"
-									id="uploadedAvatar"
-								/>
-								<div class="button-wrapper">
-									<label for="header_logo" class="btn btn-primary me-2 mb-4" tabindex="0">
-									<span class="d-none d-sm-block">Upload Header Logo</span>
-									<i class="bx bx-upload d-block d-sm-none"></i>
-									<input
-										type="file"
-										id="header_logo"
-										name="header_logo"
-										class="account-file-input"
-										hidden
-										accept="image/png, image/jpeg"
-									/>
-									</label>
-									<p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
-								</div>
-							</div>
-							<div class="d-flex align-items-start align-items-sm-center gap-4">
-								<img
-									src="../assets/img/avatars/1.png"
-									alt="user-avatar"
-									class="d-block rounded"
-									height="100"
-									width="100"
-									id="uploadedAvatar"
-								/>
-								<div class="button-wrapper">
-									<label for="footer_logo" class="btn btn-primary me-2 mb-4" tabindex="0">
-									<span class="d-none d-sm-block">Upload Footer Logo</span>
-									<i class="bx bx-upload d-block d-sm-none"></i>
-									<input
-										type="file"
-										id="footer_logo"
-										name="footer_logo"
-										class="account-file-input"
-										hidden
-										accept="image/png, image/jpeg"
-									/>
-									</label>
-									<p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
-<div class="row">
-	<div class="col-md-12">
-		@if ($errors->any())
-			<div class="alert alert-danger">
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@endif
-		@if(session()->has('message'))
-			<div class="alert alert-success" role="alert">{{ session()->get('message') }}</div>
-		@endif
-		<form id="formAccountSettings" method="POST" action="{{ route('admin-save-settings') }}" enctype="multipart/form-data" >
-			@csrf
-			<div class="nav-align-top mb-4">
-				<ul class="nav nav-pills flex-column flex-md-row mb-3" role="tablist">
-					<li class="nav-item">
-						<button
-							type="button"
-							class="nav-link active"
-							role="tab"
-							data-bs-toggle="tab"
-							data-bs-target="#navs-pills-justified-home"
-							aria-controls="navs-pills-justified-home"
-							aria-selected="true"
-						>
-							<i class="tf-icons bx bx-home"></i> General Settings
+							<i class="tf-icons bx bx-message-square"></i> Utm Settings
 						</button>
 					</li>
 					<li class="nav-item">
@@ -148,21 +63,8 @@
 							class="nav-link"
 							role="tab"
 							data-bs-toggle="tab"
-							data-bs-target="#navs-pills-justified-profile"
-							aria-controls="navs-pills-justified-profile"
-							aria-selected="false"
-						>
-							<i class="tf-icons bx bx-user"></i> Contact Settings
-						</button>
-					</li>
-					<li class="nav-item">
-						<button
-							type="button"
-							class="nav-link"
-							role="tab"
-							data-bs-toggle="tab"
-							data-bs-target="#navs-pills-justified-messages"
-							aria-controls="navs-pills-justified-messages"
+							data-bs-target="#navs-pills-justified-social-media"
+							aria-controls="navs-pills-justified-social-media"
 							aria-selected="false"
 						>
 							<i class="tf-icons bx bx-message-square"></i> Social Settings
@@ -237,29 +139,7 @@
 												<option value="1" {{ (isset($settings["enable_otp"]) && $settings["enable_otp"] == "1")?"selected":"" }}> Yes</option>
 												<option value="0" {{ (isset($settings["enable_otp"]) && $settings["enable_otp"] == "0")?"selected":"" }}> No </option>
 											</select>
-						<hr class="my-0" />
-						<div class="card-body">
-							<div class="row">
-								<div class="col-md-6">
-									<div class="row">
-										<div class="mb-3 col-12">
-											<label for="enable_otp" class="form-label">Enable Otp</label>
-											<select id="enable_otp" name="enable_otp" class="select2 form-select">
-												<option value="">Enable Otp</option>
-												<option value="1" {{ (isset($settings["enable_otp"]) && $settings["enable_otp"] == "1")?"selected":"" }}> Yes</option>
-												<option value="0" {{ (isset($settings["enable_otp"]) && $settings["enable_otp"] == "0")?"selected":"" }}> No </option>
-											</select>
 										</div>
-										<div class="mb-3 col-12">
-											<label for="firstName" class="form-label">Razorpay Key</label>
-											<input
-												class="form-control"
-												type="text"
-												name="razorpay_key" 
-												id="razorpay_key"
-												value="{{ (isset($settings['razorpay_key']))?$settings['razorpay_key']:'' }}"
-												autofocus
-											/>
 										<div class="mb-3 col-12">
 											<label for="firstName" class="form-label">Razorpay Key</label>
 											<input
@@ -272,13 +152,35 @@
 											/>
 										</div>
 										<div class="mb-3 col-12">
-											<label for="firstName" class="form-label">Razorpay Secret</label>
+											<label for="razorpay_secret" class="form-label">Razorpay Secret</label>
 											<input
 												class="form-control"
 												type="text"
 												name="razorpay_secret"
 												id="razorpay_secret" 
 												value="{{ (isset($settings['razorpay_secret']))?$settings['razorpay_secret']:'' }}"
+												autofocus
+											/>
+										</div>
+										<div class="mb-3 col-12">
+											<label for="commission_amount" class="form-label">Session Commission Amount</label>
+											<input
+												class="form-control"
+												type="text"
+												name="commission_amount"
+												id="commission_amount" 
+												value="{{ (isset($settings['commission_amount']))?$settings['commission_amount']:'' }}"
+												autofocus
+											/>
+										</div>
+										<div class="mb-3 col-12">
+											<label for="max_discount" class="form-label">Maximum discount</label>
+											<input
+												class="form-control"
+												type="text"
+												name="max_discount"
+												id="max_discount" 
+												value="{{ (isset($settings['max_discount']))?$settings['max_discount']:'' }}"
 												autofocus
 											/>
 										</div>
@@ -348,58 +250,7 @@
 												id="address" 
 												autofocus
 											>{{ (isset($settings['address']))?$settings['address']:'' }}</textarea>
-						<!-- /Account -->
-					</div>
-					<div class="tab-pane fade" id="navs-pills-justified-profile" role="tabpanel">
-						<h5 class="card-header">Contact Information</h5>
-						<div class="card-body">
-							<div class="row">
-								<div class="col-md-6">
-									<div class="row">
-										<div class="mb-3 col-md-12">
-											<label for="firstName" class="form-label">Mobile Number</label>
-											<input
-												class="form-control"
-												type="text"
-												name="mobile" id="mobile" value="{{ (isset($settings['mobile']))?$settings['mobile']:'' }}" 
-												autofocus
-											/>
-										</div>		
-										<div class="mb-3 col-md-12">
-											<label for="firstName" class="form-label">Email Address</label>
-											<input
-												class="form-control"
-												type="text"
-												name="email" id="email" value="{{ (isset($settings['email']))?$settings['email']:'' }}" 
-												autofocus
-											/>
-										</div>		
-										<div class="mb-3 col-md-12">
-											<label for="firstName" class="form-label">Whatsapp Number</label>
-											<input
-												class="form-control"
-												type="text"
-												name="whatsapp" id="whatsapp" value="{{ (isset($settings['whatsapp']))?$settings['whatsapp']:'' }}" 
-												autofocus
-											/>
-										</div>	
-										<div class="mb-3 col-md-12">
-											<label for="address" class="form-label">HO Address</label>
-											<textarea
-												class="form-control"
-												name="address"
-												id="address" 
-												autofocus
-											>{{ (isset($settings['address']))?$settings['address']:'' }}</textarea>
 										</div>
-										<div class="mb-3 col-md-12">
-											<label for="gmap" class="form-label">Google Map</label>
-											<textarea
-												class="form-control"
-												name="gmap"
-												id="gmap" 
-												autofocus
-											>{{ (isset($settings['gmap']))?$settings['gmap']:'' }}</textarea>
 										<div class="mb-3 col-md-12">
 											<label for="gmap" class="form-label">Google Map</label>
 											<textarea
@@ -414,39 +265,44 @@
 							</div>
 						</div>
                     </div>
-					<div class="tab-pane fade" id="navs-pills-justified-messages" role="tabpanel">
+					<div class="tab-pane fade" id="navs-pills-justified-utm" role="tabpanel">
 						<div class="card-body">
 							<div class="row">
 								<div class="col-md-6">
 									<div class="row">
 										<div class="mb-3 col-md-12">
-											<label for="firstName" class="form-label">Facebook</label>
+											<label for="firstName" class="form-label">UTM Campaign</label>
 											<input
 												class="form-control"
 												type="text"
-												name="facebook" id="facebook" placeholder="Enter Facebook Link Here" value="{{ (isset($settings['facebook']))?$settings['facebook']:'' }}" 
+												name="utm_campaign" id="utm_campaign" placeholder="Enter UTM Campaign Here" value="{{ (isset($settings['utm_campaign']))?$settings['utm_campaign']:'' }}" 
 												autofocus
 											/>
 										</div>		
 										<div class="mb-3 col-md-12">
-											<label for="firstName" class="form-label">Twitter</label>
+											<label for="firstName" class="form-label">Utm Source</label>
 											<input
 												class="form-control"
 												type="text"
-												name="twitter" id="twitter" placeholder="Enter Twitter Link Here" value="{{ (isset($settings['twitter']))?$settings['twitter']:'' }}"
+												name="utm_source" id="utm_source" placeholder="Enter Twitter Link Here" value="{{ (isset($settings['utm_source']))?$settings['utm_source']:'' }}"
 												autofocus
 											/>
 										</div>		
 										<div class="mb-3 col-md-12">
-											<label for="firstName" class="form-label">LinkedIn</label>
+											<label for="firstName" class="form-label">Communication Medium</label>
 											<input
 												class="form-control"
 												type="text"
-												name="linkedin" id="linkedin" placeholder="Enter Linkedin Link Here" value="{{ (isset($settings['linkedin']))?$settings['linkedin']:'' }}"
+												name="lead_type" id="lead_type" placeholder="Enter Communication Medium Here" value="{{ (isset($settings['lead_type']))?$settings['lead_type']:'' }}"
 												autofocus
 											/>
-                    </div>
-					<div class="tab-pane fade" id="navs-pills-justified-messages" role="tabpanel">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="navs-pills-justified-social-media" role="tabpanel">
 						<div class="card-body">
 							<div class="row">
 								<div class="col-md-6">
@@ -487,24 +343,11 @@
 												autofocus
 											/>
 										</div>
-										<div class="mb-3 col-md-12">
-											<label for="firstName" class="form-label">Youtube</label>
-											<input
-												class="form-control"
-												type="text"
-												name="youtube" id="youtube" placeholder="Enter YouTube Link Here" value="{{ (isset($settings['youtube']))?$settings['youtube']:'' }}"
-												autofocus
-											/>
-										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>	
-				<div class="mt-2">
-					<button type="submit" class="btn btn-primary me-2">Save changes</button>
-					<button type="reset" class="btn btn-outline-secondary">Cancel</button>
 				</div>	
 				<div class="mt-2">
 					<button type="submit" class="btn btn-primary me-2">Save changes</button>
@@ -514,7 +357,6 @@
 			</div>
 		</form>
 	</div>
-</div>
 </div>
 @endsection
 
